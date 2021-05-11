40a67916185cd5a178d7925027a91821737d7c5f
Justin Ramirez <ramirezbjustin@gmail.com>
2021-05-11 18:42:42 -0400

    adding in background images for e commerce page

--- script.js
+++ script.js
@@ -3,7 +3,11 @@ const slideshowDivs = () => {
   for (let i = 1; i <= 5; i++) {
     const div = document.createElement('div');
 
-    div.style.backgroundImage = ``;
+    div.style.backgroundImage = `url(images/slideshow/section-1-bg-${i}.jpg)`;
+
+    document.querySelector('.slideshow').appendChild(div);
   }
 };
+
+slideshowDivs();
 // End of Slideshow
