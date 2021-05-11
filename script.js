29a227efeda2d5f5f90fe2872606138f5d0d6ce6
Justin Ramirez <ramirezbjustin@gmail.com>
2021-05-11 18:36:03 -0400

    adding in images folder for e commerce website

---
+++ script.js
@@ -0,0 +1,9 @@
+// Slideshow
+const slideshowDivs = () => {
+  for (let i = 1; i <= 5; i++) {
+    const div = document.createElement('div');
+
+    div.style.backgroundImage = ``;
+  }
+};
+// End of Slideshow
