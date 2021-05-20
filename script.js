f23b1c8e96e7dbeb7d9d82fdf92c2b6bc2eeb45a
Justin Ramirez <ramirezbjustin@gmail.com>
2021-05-19 20:34:16 -0400

    finalized scroll section with bands and watches

--- script.js
+++ script.js
@@ -128,19 +128,46 @@ const watchLeftControl = document.querySelector('.watch-left-control');
 let axisY = 0;
 let axisX = 0;
 
+const hideControl = () => {
+  if (axisY === -280) {
+    watchTopControl.classList.add('hideControl');
+  } else {
+    watchTopControl.classList.remove('hideControl');
+  }
+  if (axisY === 280) {
+    watchBottomControl.classList.add('hideControl');
+  } else {
+    watchBottomControl.classList.remove('hideControl');
+  }
+  if (axisX === 280) {
+    watchRightControl.classList.add('hideControl');
+  } else {
+    watchRightControl.classList.remove('hideControl');
+  }
+  if (axisX === -280) {
+    watchLeftControl.classList.add('hideControl');
+  } else {
+    watchLeftControl.classList.remove('hideControl');
+  }
+};
+
 watchTopControl.addEventListener('click', () => {
   watchCases.style.marginTop = `${(axisY -= 70)}rem`;
+  hideControl();
 });
 
 watchBottomControl.addEventListener('click', () => {
   watchCases.style.marginTop = `${(axisY += 70)}rem`;
+  hideControl();
 });
 
 watchRightControl.addEventListener('click', () => {
   watchBands.style.marginRight = `${(axisX += 70)}rem`;
+  hideControl();
 });
 
 watchLeftControl.addEventListener('click', () => {
   watchBands.style.marginRight = `${(axisX -= 70)}rem`;
+  hideControl();
 });
 // End of Section 4
